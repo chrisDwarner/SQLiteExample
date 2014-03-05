af1e484f2ae703f714ebba1e27ecd6ee1d2b9e08
chris warner <chris@BigMac.local>
2014-03-05 11:01:42 -0800

    setup a table view

--- SQLiteExample/ViewController.m
+++ SQLiteExample/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSArray *_tableViewContents;
+}
 @end
 
 @implementation ViewController
@@ -18,12 +20,36 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // setup a dummy table cell
+    _tableViewContents = [NSArray arrayWithObjects:@"item", nil];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_tableViewContents count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [_tableViewContents objectAtIndex:indexPath.row];
+    return cell;
 }
 
 @end
